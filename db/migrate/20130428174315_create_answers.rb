529bb78885bd129996d0614bae2afcf1c5b67c1b
Yurij Bryndzia <ukegamer87@gmail.com>
2013-04-28 18:02:04 -0500

    improving

--- db/migrate/20130428174315_create_answers.rb
+++ db/migrate/20130428174315_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def up
   	create_table	:answers do |q|
-  		q.string :stuff
-  		q.string :things
+  		q.string :name
+  		q.string :feelings
   		q.string :am_activity
   		q.string :pm_activity
   		q.string :hunger_level
